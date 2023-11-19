3c7ec13c49d89fce4bbb4cfd7204639ac3bc3f60
Isaac Asiedu <asieduisaac5775@gmail.com>
2023-11-19 22:09:17 +0000

    w7 cp done

--- cse110 revisit/w7/loop_cp.py
+++ cse110 revisit/w7/loop_cp.py
@@ -1,13 +1,21 @@
+# Use a while loop to ask the user for a positive number (>= 0).
 
-# number = 0
-
-# Use a while loop to ask the user for a positive number (>= 0). 
-
-number = -1
+number = int(input("Please type a positive number: "))
 
 # Continue asking as long as the number is negative, then display the number.
 while number < 0:
-    number = int(input("Please type a positive number: "))
     print("Sorry, that is a negative number. Please try again.")
+    number = int(input("Please type a positive number: "))
+
 
 print(f"The number is: {number}")
+
+
+# Ask for candy
+
+ask_candy = "no"
+
+while ask_candy == "no":
+    ask_candy = input("May I have a piece of candy? ")
+    
+print("Thank You")
