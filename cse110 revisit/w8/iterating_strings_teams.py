ebb0b61cf153b78ecd808dcd744882b3d3c1423b
Asiedu Isaac <asieduisaac5775@gmail.com>
2023-11-29 01:00:16 +0000

    w8 teams done

---
+++ cse110 revisit/w8/iterating_strings_teams.py
@@ -0,0 +1,24 @@
+"""Write a program that asks the user for their favorite letter.
+ Then, loop through a programmed word one letter at a time. If the letter in the programmed 
+ word is the user's favorite, print it out as a capital letter (or "hide" it), if not, print 
+ it out as a lower case letter."""
+
+word = "Commitment"
+user_letter = input("What is your favorite letter? ")
+
+#  2)
+# for letter in word:
+#     if user_letter.lower() == letter:
+#         print(letter.upper(), end="") #use end="" to print on the same line.
+#     else:
+#         print(letter.lower(), end="")
+
+
+# 3)
+for letter in word:
+    if user_letter.lower() == letter:
+        print("_", end="") #use end="" to print on the same line.
+    else:
+        print(letter.lower(), end="")
+
+
