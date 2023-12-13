163407dd11f0963e7af3fbe83e544734fbf0f7a4
Asiedu Isaac <asieduisaac5775@gmail.com>
2023-12-13 16:46:30 +0000

    on lesson 10 final pro

--- cse110 revisit/w10/shopping_cart_v2.py
+++ cse110 revisit/w10/shopping_cart_v2.py
@@ -6,6 +6,7 @@ print("\nWelcome to the Shopping Cart Program!")
 menu = ["1. Add item", "2. View cart", "3. Remove item", "4. Compute total", "5. Quit"]
 
 shopping_cart = []
+prices = []
 
 user_menu = 0
 
@@ -19,16 +20,21 @@ while user_menu != 5:
     if user_menu == 1:
         item = input("What item would you like to add? ")
         price = float(input(f"What is the price of '{item}'? $"))
-        shopping_cart.append(item and price)
-        # shopping_cart.append(price)
+        shopping_cart.append(item)
+        prices.append(price)
         print(f"'{item}' has been added to the cart.")
-        # user_menu = menu[0]
 
     elif user_menu == 2:
         print("\nThe contents of the shopping cart are: ")
-        for item in shopping_cart:
-            print(f"{item} - ${price}")
-        # user_menu = menu[1]
+        # for i in enumerate(range(len(shopping_cart, start=1))):
+        #     item = shopping_cart[i]
+        #     price = prices[i]
+        #     print(f"{i}. {item} - ${price}")
+        for i, item in enumerate(shopping_cart, start=1):
+            for i in range(len(prices)):
+                price = prices[i]
+            print(f"{i}. {item} - ${price}")
+
     elif user_menu == 3:
         user_menu = menu[2]
     elif user_menu == 4:
