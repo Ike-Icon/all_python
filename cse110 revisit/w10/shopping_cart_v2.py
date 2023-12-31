ad0ec1536f3047b63973d263a1fecb0b42262eeb
Asiedu Isaac <asieduisaac5775@gmail.com>
2023-12-31 21:09:53 +0000

    cse110 done

--- cse110 revisit/w10/shopping_cart_v2.py
+++ cse110 revisit/w10/shopping_cart_v2.py
@@ -5,6 +5,7 @@
 print("\nWelcome to the Shopping Cart Program!")
 menu = ["1. Add item", "2. View cart", "3. Remove item", "4. Compute total", "5. Quit"]
 
+# Create a list that will store the names of the items in the shopping cart.
 shopping_cart = []
 prices = []
 
@@ -17,6 +18,7 @@ while user_menu != 5:
         print(opt)
     user_menu = int(input("Please enter an action: "))
 
+    # Add a new item
     if user_menu == 1:
         item = input("What item would you like to add? ")
         price = float(input(f"What is the price of '{item}'? $"))
@@ -24,25 +26,37 @@ while user_menu != 5:
         prices.append(price)
         print(f"'{item}' has been added to the cart.")
 
+    # Display the contents of the shopping cart
     elif user_menu == 2:
         print("\nThe contents of the shopping cart are: ")
-        # for i in enumerate(range(len(shopping_cart, start=1))):
-        #     item = shopping_cart[i]
-        #     price = prices[i]
-        #     print(f"{i}. {item} - ${price}")
-        for i, item in enumerate(shopping_cart, start=1):
-            for i in range(len(prices)):
-                price = prices[i]
-            print(f"{i}. {item} - ${price}")
+        # The number the user enters should be converted to a 0-based index and checked to make sure it is within the bounds of the list.
+        for i in range(1, len(shopping_cart) + 1):
+            if 1 <= i <= len(shopping_cart) and 1 <= i <= len(prices):
+                item = shopping_cart[i - 1]
+                price = prices[i - 1]
+                print(f"{i}. {item} - ${price}")
 
+    # Remove an item
     elif user_menu == 3:
-        user_menu = menu[2]
+        removeItem = int(input("\nWhich item would you like to remove? "))
+        shopping_cart.pop(removeItem - 1)
+        prices.pop(removeItem - 1)
+        print("Item Removed")
+
+        # make sure then user removes an item in the length of the shopping cart
+        # for removeItem in range(1, len(shopping_cart) + 1):
+        #     if removeItem > len(shopping_cart):
+        #         print("The item is not in the shopping cart")
+
+    # Compute the total
     elif user_menu == 4:
-        user_menu = menu[3]
+        totalAmount = 0
+        for price in prices:
+            totalAmount += price
+        print(f"The total amount is: {totalAmount:.2f}")
+
+    # Quit
     else:
         user_menu == 5
-        user_menu = menu[4]
         print("Thank you. Goodbye.")
         break
-
-# print(shopping_cart)
